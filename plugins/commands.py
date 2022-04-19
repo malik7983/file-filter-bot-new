45a4c532a25cf1394e7c94acc82d83280c144654
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-04-20 03:24:29 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -67,7 +67,7 @@ async def start(client, message):
         btn = [
             [
                 InlineKeyboardButton(
-                    "🤖 Join Updates Channel", url=invite_link.invite_link
+                    "👉 Join Updates Channel 👈", url=invite_link.invite_link
                 )
             ]
         ]
@@ -81,7 +81,7 @@ async def start(client, message):
                 btn.append([InlineKeyboardButton(" 🔄 Try Again", url=f"https://t.me/{temp.U_NAME}/{message.command[1]}")])
         await client.send_message(
             chat_id=message.from_user.id,
-            text="**Please Join My Updates Channel to use this Bot!**",
+            text="**Please Join My Updates Channel to use this Bot movie lene ke liy hamare updatechannelko join kare agar app updatechannelko join nahi karenge bot Movie nahi dega !**",
             reply_markup=InlineKeyboardMarkup(btn),
             parse_mode="markdown"
             )
