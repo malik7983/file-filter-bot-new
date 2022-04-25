c9a7877052ebc69f2854f59080d7c12b1af5ee9f
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-04-26 04:57:07 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -647,8 +647,7 @@ async def auto_filter(client, msg, spoll=False):
                 InlineKeyboardButton(
                     text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'{pre}#{file.file_id}'
                 InlineKeyboardButton('🌴 Bots Channel 🌴', url='https://t.me/malik_bots')
-                ],
-                [
+                ],[   
             ]
             for file in files
         ]
@@ -659,8 +658,8 @@ async def auto_filter(client, msg, spoll=False):
                     text=f"{file.file_name}",
                     callback_data=f'{pre}#{file.file_id},
                 InlineKeyboardButton('🌴 Bots Channel 🌴', url='https://t.me/malik_bots')
-                ],
-                [
+                ],[
+            ]
                 InlineKeyboardButton(
                     text=f"{get_size(file.file_size)}",
                     callback_data=f'{pre}_#{file.file_id}',
