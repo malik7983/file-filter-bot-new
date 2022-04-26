5ed5e246f497a42421af0f4d493c6eaed6bb6c67
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-04-26 05:44:52 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -344,14 +344,14 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 f_caption = CUSTOM_FILE_CAPTION.format(file_name='' if title is None else title,
                                                        file_size='' if size is None else size,
                                                        file_caption='' if f_caption is None else f_caption)
-         buttons = [
-             [
-                  InlineKeyboardButton('🖥️ How To Own 🖥️', url=f'{TUTORIAL}')
-             ],
-             [
-                  InlineKeyboardButton('🔍 Search again 🔎', switch_inline_query_current_chat='')
-             ]
-             ]
+                buttons = [
+                    [
+                        InlineKeyboardButton('🖥️ How To Own 🖥️', url=f'{TUTORIAL}')
+                    ],
+                    [
+                        InlineKeyboardButton('🔍 Search again 🔎', switch_inline_query_current_chat='')
+                    ]
+                    ]
             except Exception as e:
                 logger.exception(e)
             f_caption = f_caption
