f1af893a97ee9218ba659c15672141f864d9c4a0
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-05-01 16:28:03 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -672,9 +672,13 @@ async def auto_filter(client, msg, spoll=False):
     if settings["button"]:
         btn = [
             [
+                InlineKeyboardButton('HOW TO DOWNLOAD', url='https://youtu.be/NoZOf8q0QzQ'),
+                ),
+            ]
+        btn = [
+            [
                 InlineKeyboardButton(
                     text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'{pre}#{file.file_id}'
-                InlineKeyboardButton('HOW TO DOWNLOAD', url='https://youtu.be/NoZOf8q0QzQ'),
                 ),
             ]
             for file in files
@@ -685,7 +689,6 @@ async def auto_filter(client, msg, spoll=False):
                 InlineKeyboardButton(
                     text=f"{file.file_name}",
                     callback_data=f'{pre}#{file.file_id}',
-                InlineKeyboardButton('HOW TO DOWNLOAD', url='https://youtu.be/NoZOf8q0QzQ'),
                 ),
                 InlineKeyboardButton(
                     text=f"{get_size(file.file_size)}",
