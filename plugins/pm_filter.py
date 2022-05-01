1fae2aeb8e359de5d6647094a91ec1d851e9abe8
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-05-01 09:36:14 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -65,8 +65,7 @@ async def next_page(bot, query):
             [
                 InlineKeyboardButton(
                     text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'files#{file.file_id}'
-                ),
-                [
+                )
                 InlineKeyboardButton('🌴 Bots Channel 🌴', url='https://t.me/malik_bots'),
                 ),
             ]
