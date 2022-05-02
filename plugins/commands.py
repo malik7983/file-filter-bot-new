e49ebbd3210244ba6580f60a85a3a548c8b139b7
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-05-02 15:18:04 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -226,12 +226,6 @@ async def start(client, message):
             title = file.file_name
             size=get_size(file.file_size)
             f_caption = f"<code>{title}</code"
-            return
-            [
-                InlineKeyboardButton("👉 Join Updates Channel 👈", url=https:t.me/malik_bots),
-                )
-            ]
-        ]   
             if CUSTOM_FILE_CAPTION:
                 try:
                     f_caption=CUSTOM_FILE_CAPTION.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='')
