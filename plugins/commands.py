8caf482bdbcda2183342e334ea46787d92d9a69c
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-05-02 15:08:52 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -225,7 +225,7 @@ async def start(client, message):
             file = getattr(msg, filetype)
             title = file.file_name
             size=get_size(file.file_size)
-            f_caption = f"<code>{title}</code"
+            f_caption = f"<code>{title}</code"),
         btn = [
             [
                 InlineKeyboardButton(
