a39642b5056be46bca5df4a3be9a478b05be067e
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-05-05 21:19:13 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -672,6 +672,7 @@ async def auto_filter(client, msg, spoll=False):
     if settings["button"]:
         btn = [
             [
+                InlineKeyboardButton('💠 CHANNEL 💠', url='https://t.me/+ZeZNvt43B4o3ZmJl'),
                 InlineKeyboardButton(
                     text=f"🖥 [{get_size(file.file_size)}] {file.file_name}", callback_data=f'{pre}#{file.file_id}'
                 ),
