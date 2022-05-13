12a9109e1605817f9d6cad250c2007540713d4eb
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-05-13 20:26:47 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -584,6 +584,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
             reply_markup=reply_markup,
             parse_mode='html'
         )
+    elif query.data == "stats":
+        buttons = [[
             InlineKeyboardButton('🚶 𝗕𝗮𝗰𝗸 🚶', callback_data='help'),
             InlineKeyboardButton('♻️', callback_data='rfrsh')
         ]]
