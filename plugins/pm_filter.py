ce69c3023fb1b03572d176956d6aeb4e0588f22b
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-05-13 20:53:34 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -565,7 +565,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             reply_markup=reply_markup,
             parse_mode='html'
         )
-    elif query.data == "MALIK":
+    elif query.data == "malik":
         buttons = [[
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
