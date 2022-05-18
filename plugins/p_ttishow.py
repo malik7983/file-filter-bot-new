c10391283a3ab31777750f844081bc24367cc9b4
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-05-18 11:34:37 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -52,10 +52,10 @@ async def save_group(bot, message):
             for u in message.new_chat_members:
                 if (temp.MELCOW).get('welcome') is not None:
                     try:
-                        await (temp.MELCOW['welcome']).delete()
+                        await (temp.MELCOW['welcome']).delete(10)
                     except:
                         pass
-                temp.MELCOW['welcome'] = await message.reply(f"<b>Hey , {u.mention}, Welcome to {message.chat.title}</b>")
+                temp.MELCOW['welcome'] = await message.reply(f"<b>Hey , {u.mention}, Welcome to {message.chat.title}You Can Find Movies / Series / Animes etc. From Here. Enjoy 😉.</b>")
 
 
 @Client.on_message(filters.command('leave') & filters.user(ADMINS))
