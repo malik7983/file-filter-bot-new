deb0e482f873e6ebc65c0a63d8f92f15bb715e14
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-05-22 18:59:23 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -59,14 +59,13 @@ async def save_group(bot, message):
                     try:
                      m = await (temp.MELCOW['welcome']).delete()
                     except:
-                    await asyncio.sleep(20)
-                    await m.delete()
                         pass
                 temp.MELCOW['welcome'] = await message.reply_text(
                 text=f"<b>Hey 👋 {u.mention}, Welcome to Our Group {message.chat.title} You Can Find Movies / Series / Animes etc. From Here. Enjoy 😉.</b> \n\n<b>If you have any question then contact us below 👇</b>",
                 disable_web_page_preview = True,
                 reply_markup=InlineKeyboardMarkup(buttons))
-
+                    await asyncio.sleep(20)
+                    await m.delete()
 
 @Client.on_message(filters.command('leave') & filters.user(ADMINS))
 async def leave_a_chat(bot, message):
