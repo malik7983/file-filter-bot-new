96cab276e148b8802c896345efd8ea4e9b42ebfd
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-05-25 13:57:21 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -42,7 +42,7 @@ async def next_page(bot, query):
 
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer("😎 Hey. Hey, {query.from_user.first_name} \n\n   This is not for you. \n\n\n ⚠️ Note : If You Want This You Can Request Your Own", show_alert=True)
+        return await query.answer("⚠️ Hey, {query.from_user.first_name}! Search Your Own File, Don't Click Others Results 😬", show_alert=True)
     try:
         offset = int(offset)
     except:
@@ -133,7 +133,7 @@ async def next_page(bot, query):
 async def advantage_spoll_choker(bot, query):
     _, user, movie_ = query.data.split('#')
     if int(user) != 0 and query.from_user.id != int(user):
-        return await query.answer("😎 Hey, {query.from_user.first_name} \n\n   This is not for you. \n\n\n ⚠️ Note : If You Want This You Can Request Your Own", show_alert=True)
+        return await query.answer("⚠️ Hey, {query.from_user.first_name}! Search Your Own File, Don't Click Others Results 😬", show_alert=True)
     if movie_ == "close_spellcheck":
         return await query.message.delete()
     movies = SPELL_CHECK.get(query.message.reply_to_message.message_id)
