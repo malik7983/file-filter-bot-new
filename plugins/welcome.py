464ee225f777d0baa1fe64e7c2339177b2ed1182
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-05-25 17:30:30 +0530

    Update welcome.py

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -12,5 +12,5 @@ async def welcome(bot,message):
 @Client.on_message(filters.left_chat_member)
 async def goodbye(bot,message):
 	chatid= message.chat.id
-        await bot.send_message(text=f"Bye 👋,  {message.from_user.mention} , Have a Nice Day",chat_id=chatid)
-  
+	await bot.send_message(text=f"Bye ,  {message.from_user.mention} , Have a Nice Day",chat_id=chatid)
+	
