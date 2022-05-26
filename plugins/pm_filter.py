96e7f47c82aec22fca78348bff044bbca6416f71
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-05-26 09:36:25 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -819,7 +819,10 @@ async def advantage_spell_chok(msg):
         hmm = InlineKeyboardMarkup(
         [
             [
-                 InlineKeyboardButton("🕵️‍♂️ Search in Google 🕵️‍♂️", url=f"https://google.com/search?q={search}%20{search}%20{search}")
+                 InlineKeyboardButton("🔍 Search in Google 🔎", url=f"https://google.com/search?q={search}"),
+                 ],
+                 [
+                 InlineKeyboardButton(" Search in Google 📆", url=f"https://google.com/search?q={search}")
             ]
         ]
     )
