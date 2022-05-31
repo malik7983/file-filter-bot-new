2d09e92d86f250a93ffaa1d6afb8a6a44b0b1dbe
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-05-31 11:10:13 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -61,7 +61,7 @@ async def save_group(bot, message):
                     except:
                         pass
                 temp.MELCOW['welcome'] = await message.reply_text(
-                text=f"<b>Hey 👋 {u.mention}, Welcome to Our Group {message.chat.title} You Can Find Movies / Series / Animes etc. From Here. Enjoy 😉.</b> \n\n<b>If you have any question then contact us below 👇</b>",
+                text=f"<b>Hey 👋 {u.mention},\n\nWelcome to Our Group 👉 {message.chat.title}.. \nYou Can Find Movies / Series / Animes etc. From Here. Enjoy 😉.</b> \n\n<b>If you have any question then contact us below 👇</b>",
                 disable_web_page_preview = True,
                 reply_markup=InlineKeyboardMarkup(buttons))
         await asyncio.sleep(16)
