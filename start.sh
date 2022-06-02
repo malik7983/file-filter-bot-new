3b32cf03c78068ad039c3c10eeefb94d721b0627
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-06-02 09:46:01 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/malik7983/file-filter-bot-new.git /file-filter-bot-new
+  git clone https://github.com/malik7983/Ajax-Extra-Features.git /Ajax-Extra-Features
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /file-filter-bot-new
+  git clone $UPSTREAM_REPO /Ajax-Extra-Features
 fi
-cd /file-filter-bot-new
+cd /Ajax-Extra-Features
 pip3 install -U -r requirements.txt
 echo "Starting Bot....💥"
 python3 bot.py
