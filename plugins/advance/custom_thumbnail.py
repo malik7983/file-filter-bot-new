4550dfa907eaee3ee5d90edc88bb5d2579d66074
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-06-06 22:17:33 +0530

    Update custom_thumbnail.py

--- plugins/advance/custom_thumbnail.py
+++ plugins/advance/custom_thumbnail.py
@@ -3,8 +3,6 @@ import time
 
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
-else:
-    from config import Config
 
 from PIL import Image
 from pyrogram import Client, filters
