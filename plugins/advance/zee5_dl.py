c838b6d7e83659698cabc41f729c0947af826486
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-06-06 22:37:45 +0530

    Update zee5_dl.py

--- plugins/advance/zee5_dl.py
+++ plugins/advance/zee5_dl.py
@@ -16,8 +16,6 @@ import requests
 
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
-else:
-    from config import Config
 
 from script import script
 from database.database import *
