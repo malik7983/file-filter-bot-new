18038c96d26fac08f7e4c189d314a3163a2f5742
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-06-07 19:27:27 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -64,8 +64,8 @@ async def save_group(bot, message):
                 text=f"<b>Hey 👋 {u.mention}, Welcome to Our Group 👉 <s>{message.chat.title}</s>.. You Can Find Movies / Series / Animes etc. From Here. Enjoy 😉.</b> \n\n<b>If you have any question then contact us below 👇</b>",
                 disable_web_page_preview = True,
                 reply_markup=InlineKeyboardMarkup(buttons))
-        await asyncio.sleep(16)
-        await b.delete()
+                await message.reply_photo(
+                    photo=random.choice(PICS),
 
 @Client.on_message(filters.command('leave') & filters.user(ADMINS))
 async def leave_a_chat(bot, message):
