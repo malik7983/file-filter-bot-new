104c22b06e4c86787b0e44e9b8a7a005f3cc65dc
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-06-07 20:40:25 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -61,7 +61,7 @@ async def save_group(bot, message):
                     except:
                         pass
                 temp.MELCOW['welcome'] = await message.reply_photo(
-                                                 photo=random.choice(MALIK_KING),
+                                                 photo=random.choice(PIC),
                                                  caption=(MELCOW_ENG.format(u.mention, message.chat.title)),
                                                  reply_markup=InlineKeyboardMarkup(
                                                                          [[
