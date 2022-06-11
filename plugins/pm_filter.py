4d2ebf661840044cef3a9a80a1ad36371c5a912a
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-06-11 15:09:35 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -838,7 +838,7 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f"<b>Hey...😎 {message.from_user.mention},\n\nHere is the result for your query {search}\n\n🖥 Movie Name : {search}\n🎭 Genres: {genres}\n📆 Year: <a href={url}\n🌟 Rating: <a href={url}\n📡Group : {message.chat.title}\n🤦Requested By : {message.from_user.mention}</b>"
+        cap = f"<b>Hey...😎 {message.from_user.mention},\n\nHere is the result for your query {search}\n\n🖥 Movie Name : {search}\n📡Group : {message.chat.title}\n🤦Requested By : {message.from_user.mention}</b>"
     if imdb and imdb.get('poster'):
         try:
             hehe = await message.reply_photo(photo=imdb.get('poster'), caption=cap, reply_markup=InlineKeyboardMarkup(btn))
