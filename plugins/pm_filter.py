a0f2a7ca23a9e9a1ce4d8e47a514c3c9d2d263ca
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-06-16 17:54:08 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -140,6 +140,7 @@ async def advantage_spoll_choker(bot, query):
         else:
             k = await query.message.edit(M_NT_FND)
         btn.append(
+            [
                 InlineKeyboardButton('🙏 Subscribe my YouTube channel 🙏', url='https://youtube.com/channel/UCPaHDqWf3D3w2nxb8p3sr4A')
             ],
         )
