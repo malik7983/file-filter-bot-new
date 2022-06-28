f15ed5301977f9b4d2630ffbf3e89f95b18dc623
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-06-28 23:19:01 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -26,9 +26,8 @@ async def start(client, message):
                 InlineKeyboardButton('❇️ Add Me To Your Groups ❇️', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],
             [
-                InlineKeyboardButton('♻️ Updates Channel ♻️', url='https://t.me/m_house786')
-            ],[
-                InlineKeyboardButton('❇️ Help ❇️', url=f"https://t.me/{temp.U_NAME}?start=help"),
+                InlineKeyboardButton('♻️ Updates Channel ♻️', url='https://t.me/m_house786'),
+                InlineKeyboardButton('❇️ Help ❇️', url=f"https://t.me/{temp.U_NAME}?start=help")
             ]
             ]
         reply_markup = InlineKeyboardMarkup(buttons)
