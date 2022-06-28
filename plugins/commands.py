8e074224ea86d8c81071b6e42a8462cea8f719d6
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-06-28 21:28:37 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -35,7 +35,8 @@ async def start(client, message):
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
         photo=(PHTT),
-        caption=(ADDG.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup)
+        caption=(ADDG.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME)),
+        reply_markup=reply_markup)
         await asyncio.sleep(2) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
         if not await db.get_chat(message.chat.id):
             total=await client.get_chat_members_count(message.chat.id)
