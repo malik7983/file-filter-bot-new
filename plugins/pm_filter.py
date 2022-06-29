bd1503ba095eed493cbaa098d45ca6e7a39a6f96
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-06-29 21:29:43 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -138,7 +138,7 @@ async def advantage_spoll_choker(bot, query):
             k = (movie, files, offset, total_results)
             await auto_filter(bot, query, k)
         else:
-            k = await query.message.edit(M_NT_FND)
+            k = await query.message.edit(M_NT_FND),
                               reply_markup=InlineKeyboardMarkup(
                                                       [[
                                                         InlineKeyboardButton('♻️ Request to admin ♻️', url="https://t.me/m_admins")                             
