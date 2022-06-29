e7bfdf29839ba16e6ffe6905cdc1f4a9b3e3a96f
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-06-29 14:27:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -27,7 +27,7 @@ VIDEO_VD = environ.get("VIDEO_VD", "https://telegra.ph/file/566ff238e36d9f242556
 M_N_F = environ.get("M_N_F", "https://telegra.ph/file/7cf564b255461abfc75fe.jpg")
 PHT = environ.get("PHT", "https://telegra.ph/file/9b77b96a9d2f5dda7764b.jpg")
 PHTT = environ.get("PHTT", "https://telegra.ph/file/7dc82878492b8f64bb7eb.jpg")
-M_NT_F = environ.get("M_NT_F", "https://telegra.ph/file/5720c03f8eef07276a3aa.jpg")
+M_NT_F = environ.get("M_NT_F", "https://telegra.ph/file/617049055ac50169d7e2f.jpg")
 
 
 # Admins, Channels & Users
