0f568d22e9235da3a0573571585a05e6d99c1526
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-07-02 23:05:34 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -922,7 +922,7 @@ async def advantage_spell_chok(msg):
     if not g_s:
         await query.message.edit_text(
         chat_id = update.chat.id,
-        text=Text.SPELLING_TEXT.format(update.from_user.mention, the_query, the_query),
+        text=script.SPELLING_TEXT.format(update.from_user.mention, the_query, the_query),
         parse_mode="html",
         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🔍 Search Google 🔎", url="https://google.com/")]]),
         reply_to_message_id=update.message_id
@@ -952,7 +952,7 @@ async def advantage_spell_chok(msg):
     if not movielist:
         await query.message.edit_text(
         chat_id = update.chat.id,
-        text=Text.SPELLING_TEXT.format(update.from_user.mention, the_query, the_query),
+        text=script.SPELLING_TEXT.format(update.from_user.mention, the_query, the_query),
         parse_mode="html",
         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🔍 Search Google 🔎", url="https://google.com/")]]),
         reply_to_message_id=update.message_id
