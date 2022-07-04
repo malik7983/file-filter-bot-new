b8f67b77a3593f7fe1c23e4e6b8aa054c28b5c6a
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-07-04 23:19:22 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -41,7 +41,7 @@ async def next_page(bot, query):
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
         return await query.reply_text(
-               text=(M_NNT_FNDD.format(query.from_user.first_name), show_alert=True)
+               text==(M_NNT_FNDD.format(query.from_user.first_name), show_alert=True)
                reply_markup=InlineKeyboardMarkup(
                                       [[
                                         InlineKeyboardButton('💢 Request to admin 💢', url="https://t.me/m_admins"),
