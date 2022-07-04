241bdd192d4732929bd9adb0c4b67bd439d6797a
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-07-04 23:15:22 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -38,6 +38,9 @@ async def give_filter(client, message):
 
 @Client.on_callback_query(filters.regex(r"^next"))
 async def next_page(bot, query):
+    ident, req, key, offset = query.data.split("_")
+    if int(req) not in [query.from_user.id, 0]:
+        return await query.answer(
                text=(M_NNT_FNDD.format(query.from_user.first_name), show_alert=True)
                reply_markup=InlineKeyboardMarkup(
                                       [[
@@ -47,10 +50,6 @@ async def next_page(bot, query):
                ),
                parse_mode='html'
     )
-
-    ident, req, key, offset = query.data.split("_")
-    if int(req) not in [query.from_user.id, 0]:
-        return await query.answer(
 
     try:
         offset = int(offset)
