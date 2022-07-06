726c9efa525d98d10143e342c8f49d3ed1a11603
malik7983 <103699299+malik7983@users.noreply.github.com>
2022-07-06 07:55:43 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -920,7 +920,7 @@ async def advantage_spell_chok(msg):
     g_s += await search_gagala(msg.text)
     gs_parsed = []
     if not g_s:
-        hmm = await msg.reply_text(" ", '+'),
+        hmm = await msg.reply_text(" ", '+',
         hmm = InlineKeyboardMarkup(
         [
             [
