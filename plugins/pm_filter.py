da88398a812f4d3c59018be6b40aaa322328fae4
sahid malik <103699299+malik7983@users.noreply.github.com>
2022-07-13 08:30:43 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -932,12 +932,11 @@ async def advantage_spell_chok(msg):
     g_s += await search_gagala(msg.text)
     gs_parsed = []
     if not g_s:
-          reply = search.replace(" ", '+')  
-          btn = [   
-                  InlineKeyboardButton("🔍 𝚂𝙴𝙰𝚁𝙲𝙷 𝚃𝙾 𝙶𝙾𝙾𝙶𝙻𝙴 🔎", url=f"https://www.google.com/search?q={reply}"),
-                  InlineKeyboardButton("× 𝙲𝙻𝙾𝚂𝙴 ×", callback_data="close")
-                ]
-            )
+        reply = search.replace(" ", '+')  
+            [   
+               InlineKeyboardButton("🔍 𝚂𝙴𝙰𝚁𝙲𝙷 𝚃𝙾 𝙶𝙾𝙾𝙶𝙻𝙴 🔎", url=f"https://www.google.com/search?q={reply}"),
+               InlineKeyboardButton("× 𝙲𝙻𝙾𝚂𝙴 ×", callback_data="close")
+            ]
         )
     )
                   sp = await update.reply_text(text=settings["spelltext"].format(query=search, first_name=update.from_user.first_name, last_name=update.from_user.last_name, title=update.chat.title, mention=update.from_user.mention), disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(buttons))           
