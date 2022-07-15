aa36438038e6dc690cab00dcb2fc548184a54bd5
sahid malik <103699299+malik7983@users.noreply.github.com>
2022-07-15 13:30:44 +0530

    Update extra.py

--- plugins/malik/extra.py
+++ plugins/malik/extra.py
@@ -18,20 +18,7 @@ import base64
 @Client.on_message(filters.command("star") & filters.incoming & ~filters.edited)
 async def star(client, message):
     if message.chat.type in ['group', 'supergroup']:
-        buttons = [
-            [
-                InlineKeyboardButton('❇️ Add Me To Your Groups ❇️', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-            ],
-            [
-                InlineKeyboardButton('♻️ Updates Channel ♻️', url='https://t.me/m_house786'),
-                InlineKeyboardButton('❇️ Help ❇️', url=f"https://t.me/{temp.U_NAME}?start=help")
-            ]
-            ]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_video(
-        video=(PHTT),
-        caption=(ADDG.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME)),
-        reply_markup=reply_markup)
+
         await asyncio.sleep(2) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
         if not await db.get_chat(message.chat.id):
             total=await client.get_chat_members_count(message.chat.id)
