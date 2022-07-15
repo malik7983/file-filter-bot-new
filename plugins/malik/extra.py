aeb6f8d910a7504b5662097dcc841d04f50b9423
sahid malik <103699299+malik7983@users.noreply.github.com>
2022-07-15 13:15:29 +0530

    Update extra.py

--- plugins/malik/extra.py
+++ plugins/malik/extra.py
@@ -1,10 +1,20 @@
 #malik
 import os
 import logging
+import random
 import asyncio
 from Script import script, ADDG
-from info import PHTT 
-
+from pyrogram import Client, filters
+from pyrogram.errors import ChatAdminRequired, FloodWait
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from database.ia_filterdb import Media, get_file_details, unpack_new_file_id
+from database.users_chats_db import db
+from info import CHANNELS, ADMINS, AUTH_CHANNEL, PHTT, LOG_CHANNEL, PICS, BATCH_FILE_CAPTION, CUSTOM_FILE_CAPTION, PROTECT_CONTENT
+from utils import get_settings, get_size, is_subscribed, save_group_settings, temp
+from database.connections_mdb import active_connection
+import re
+import json
+import base64
 @Client.on_message(filters.command("star") & filters.incoming & ~filters.edited)
 async def star(client, message):
     if message.chat.type in ['group', 'supergroup']:
