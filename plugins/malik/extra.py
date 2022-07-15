ee5ffb26870bfc980530024dc267d0e0ef1d385d
sahid malik <103699299+malik7983@users.noreply.github.com>
2022-07-15 13:58:02 +0530

    Update extra.py

--- plugins/malik/extra.py
+++ plugins/malik/extra.py
@@ -34,8 +34,8 @@ async def star(client, message):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
-            photo=random.choice(PICS),
-            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+            photo=(PHTT),
+            caption=script.GHHM_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode='html'
         )
