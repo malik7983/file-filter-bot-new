2ad4a608665612d15a2f51a24c230b306d8b8662
sahid malik <103699299+malik7983@users.noreply.github.com>
2022-07-15 22:49:03 +0530

    Update extraa.py

--- plugins/malik/extraa.py
+++ plugins/malik/extraa.py
@@ -1,3 +1,17 @@
 
 
-GHHMT = """MALIK"""
+GHHMT = """<b>7k User 💖 Thanks For Your Support...
+
+𝖩𝗎𝗌𝗍 𝖠𝖽𝖽 𝖮𝗎𝗋 𝖡𝗈𝗍 𝖳𝗈 𝖸𝗈𝗎𝗋 𝖦𝗋𝗈𝗎𝗉 𝖠𝗌 𝖠𝖽𝗆𝗂𝗇, 𝖨𝗍 𝖶𝗂𝗅𝗅 𝖯𝗋𝗈𝗏𝗂𝖽𝖾 𝖬𝗈𝗏𝗂𝖾𝗌 𝖳𝗁𝖾𝗋𝖾... 😎
+
+
+     ♋️ 𝗙𝗲𝗮𝘁𝘂𝗿𝗲𝘀 ♋️
+
+✪ AutoFilter, Manual Filter
+✪ IMDb HD Posters
+✪ IMDb Real Details
+✪ Two Buttons Mode
+✪ Force Subscribe
+✪ Extra Features: download songs, download you tube video, URL Shortner,  
+
+⚙ More Features Adding Soon</b> 😎"""
