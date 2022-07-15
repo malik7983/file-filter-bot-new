e6cf00962d65b10620c2dfb940d848e83c898699
sahid malik <103699299+malik7983@users.noreply.github.com>
2022-07-15 12:46:57 +0530

    Create extra.py

---
+++ plugins/malik/extra.py
@@ -0,0 +1,18 @@
+@Client.on_message(filters.command("start") & filters.incoming & ~filters.edited)
+async def start(client, message):
+    if message.chat.type in ['group', 'supergroup']:
+        buttons = [
+            [
+                InlineKeyboardButton('❇️ Add Me To Your Groups ❇️', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            ],
+            [
+                InlineKeyboardButton('♻️ Updates Channel ♻️', url='https://t.me/m_house786'),
+                InlineKeyboardButton('❇️ Help ❇️', url=f"https://t.me/{temp.U_NAME}?start=help")
+            ]
+            ]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await message.reply_video(
+        video=(PHTT),
+        caption=(ADDG.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME)),
+        reply_markup=reply_markup)
+     return 
