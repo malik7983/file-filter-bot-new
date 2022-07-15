e1a41a6bc9ed9453ea103b4922a8af850125ad89
sahid malik <103699299+malik7983@users.noreply.github.com>
2022-07-15 21:46:31 +0530

    Update extra.py

--- plugins/malik/extra.py
+++ plugins/malik/extra.py
@@ -12,8 +12,6 @@ from utils import get_settings, get_size, is_subscribed, save_group_settings, te
 
 @Client.on_message(filters.command("star") & filters.incoming & ~filters.edited)
 async def star(client, message):
-    if not await db.is_user_exist(message.from_user.id):
-        await db.add_user(message.from_user.id, message.from_user.first_name)
     if len(message.command):
         buttons = [[
             InlineKeyboardButton('❇️ Add Me To Your Groups ❇️', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
