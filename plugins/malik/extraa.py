3134d34a3ead35c5caaaa7b6808e47acfdf09a34
sahid malik <103699299+malik7983@users.noreply.github.com>
2022-07-16 11:36:35 +0530

    Update extraa.py

--- plugins/malik/extraa.py
+++ plugins/malik/extraa.py
@@ -15,3 +15,10 @@ GHHMT = """<b>{}.User 💖 Thanks For Your Support...
 ✪ Extra Features: download songs, download you tube video, URL Shortner,  
 
 ⚙ More Features Adding Soon</b> 😎"""
+
+  STTS = """★ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
+★ <b>𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
+★ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
+★ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
+★ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
+★ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/sahid_malik>Sahid malik</a></b> """
