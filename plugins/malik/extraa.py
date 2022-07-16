03cade86da742a13ec6d9762933d855797aac716
sahid malik <103699299+malik7983@users.noreply.github.com>
2022-07-16 11:41:21 +0530

    Update extraa.py

--- plugins/malik/extraa.py
+++ plugins/malik/extraa.py
@@ -16,7 +16,7 @@ GHHMT = """<b>{}.User 💖 Thanks For Your Support...
 
 ⚙ More Features Adding Soon</b> 😎"""
 
-  STTS = """★ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
+STTS = """★ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
 ★ <b>𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
 ★ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
 ★ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
