e190dc69b1e791572c7f5bda9d135ac680d4d9e1
sahid malik <103699299+malik7983@users.noreply.github.com>
2022-07-18 23:45:26 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -758,7 +758,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         monsize = get_size(monsize)
         free = get_size(free)
         await query.message.edit_text(
-            text=(STTS.format(total, users, chats, monsize, free),
+            text=(STTS.format(total, users, chats, monsize, free)),
             reply_markup=reply_markup,
             parse_mode='html'
         )
@@ -777,7 +777,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         monsize = get_size(monsize)
         free = get_size(free)
         await query.message.edit_text(
-            text=script.STATUS_TXT.format(total, users, chats, monsize, free)),
+            text=(STTS.format(total, users, chats, monsize, free)),
             reply_markup=reply_markup,
             parse_mode='html'
         )
