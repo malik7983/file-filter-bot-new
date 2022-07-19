23c481d62c04807fd7b86ecea662c56bdd69ae51
sahid malik <103699299+malik7983@users.noreply.github.com>
2022-07-19 22:22:16 +0530

    Update extra.py

--- plugins/malik/extra.py
+++ plugins/malik/extra.py
@@ -7,7 +7,6 @@ from Script import script
 import time
 from typing import List
 from pyrogram.types import Message, ChatPermissions, InlineKeyboardButton
-from plugins.malik.admin_check import admin_check
 from database.users_chats_db import db
 from database.ia_filterdb import Media
 from pyrogram import Client, filters
@@ -388,6 +387,39 @@ def format_welcome_caption(html_string, chat_member):
         mention=chat_member.mention,
         username=chat_member.username
     )
+
+# admin py
+async def admin_check(message: Message) -> bool:
+    if not message.from_user:
+        return False
+
+    if message.chat.type not in ["supergroup", "channel"]:
+        return False
+
+    if message.from_user.id in [
+        777000,  # Telegram Service Notifications
+        1087968824  # GroupAnonymousBot
+    ]:
+        return True
+
+    client = message._client
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+
+    check_status = await client.get_chat_member(
+        chat_id=chat_id,
+        user_id=user_id
+    )
+    admin_strings = [
+        "creator",
+        "administrator"
+    ]
+    # https://git.colinshark.de/PyroBot/PyroBot/src/branch/master/pyrobot/modules/admin.py#L69
+    if check_status.status not in admin_strings:
+        return False
+    else:
+        return True
+
 
 
 
