d5ff04e76aadea6d40ad7222fe3389f61c64ab2c
sahid malik <103699299+malik7983@users.noreply.github.com>
2022-07-19 23:02:05 +0530

    Update extra.py

--- plugins/malik/extra.py
+++ plugins/malik/extra.py
@@ -4,7 +4,7 @@ import os
 import pyrogram
 import asyncio
 from os import environ
-from info import PHT, ADMINS, AUTH_USERS, TG_MAX_SELECT_LEN
+from info import PHT, ADMINS, AUTH_USERS
 from Script import script
 import time
 from typing import List
@@ -575,5 +575,5 @@ Thanks For Your Support...
 
 PPC = environ.get("PPC", "https://telegra.ph/file/3b6afd6c6fcd09606ea9f.jpg")
 COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
-
+TG_MAX_SELECT_LEN = environ.get("TG_MAX_SELECT_LEN", "100")
 
