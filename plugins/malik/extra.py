c79a0a9de5b867ad6e21ead04aad014376ad6852
sahid malik <103699299+malik7983@users.noreply.github.com>
2022-07-19 20:51:18 +0530

    Update extra.py

--- plugins/malik/extra.py
+++ plugins/malik/extra.py
@@ -42,6 +42,88 @@ async def get_ststs(bot, message):
                parse_mode='html'
 )
 
+@Client.on_message(filters.command("ban"))
+async def ban_user(_, message):
+    is_admin = await admin_check(message)
+    if not is_admin:
+        return
+
+    user_id, user_first_name = extract_user(message)
+
+    try:
+        await message.chat.kick_member(
+            user_id=user_id
+        )
+    except Exception as error:
+        await message.reply_text(
+            str(error)
+        )
+    else:
+        if str(user_id).lower().startswith("@"):
+            await message.reply_text(
+                "Someone else is dusting off..! "
+                f"{user_first_name}"
+                " Is forbidden."
+            )
+        else:
+            await message.reply_text(
+                "Someone else is dusting off..! "
+                f"<a href='tg://user?id={user_id}'>"
+                f"{user_first_name}"
+                "</a>"
+                " Is forbidden."
+            )
+# ban py
+
+@Client.on_message(filters.command("tban"))
+async def temp_ban_user(_, message):
+    is_admin = await admin_check(message)
+    if not is_admin:
+        return
+
+    if not len(message.command) > 1:
+        return
+
+    user_id, user_first_name = extract_user(message)
+
+    until_date_val = extract_time(message.command[1])
+    if until_date_val is None:
+        await message.reply_text(
+            (
+                "Invalid time type specified. "
+                "Expected m, h, or d, Got it: {}"
+            ).format(
+                message.command[1][-1]
+            )
+        )
+        return
+
+    try:
+        await message.chat.kick_member(
+            user_id=user_id,
+            until_date=until_date_val
+        )
+    except Exception as error:
+        await message.reply_text(
+            str(error)
+        )
+    else:
+        if str(user_id).lower().startswith("@"):
+            await message.reply_text(
+                "Someone else is dusting off..! "
+                f"{user_first_name}"
+                f" banned for {message.command[1]}!"
+            )
+        else:
+            await message.reply_text(
+                "Someone else is dusting off..! "
+                f"<a href='tg://user?id={user_id}'>"
+                "Lavane"
+                "</a>"
+                f" banned for {message.command[1]}!"
+            )
+
+
 STTS = """<b>🗂𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
 👨‍👩‍👧‍👧 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
 🤿 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
