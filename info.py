a2e52cc02cae337da1a4ac1ec0c15168394e11c7
sahid malik <103699299+malik7983@users.noreply.github.com>
2022-07-19 23:19:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -29,6 +29,8 @@ PHT = environ.get("PHT", "https://telegra.ph/file/9b77b96a9d2f5dda7764b.jpg")
 PHTT = environ.get("PHTT", "https://telegra.ph/file/1ddc551c64a4afd5a660e.mp4")
 M_NT_F = environ.get("M_NT_F", "https://telegra.ph/file/617049055ac50169d7e2f.jpg")
 
+# Command
+COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
