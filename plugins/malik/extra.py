290ca34209ec756060674e129cb1590709b3f82e
sahid malik <103699299+malik7983@users.noreply.github.com>
2022-07-19 23:31:59 +0530

    Update extra.py

--- plugins/malik/extra.py
+++ plugins/malik/extra.py
@@ -448,6 +448,99 @@ async def report_user(bot, message):
         if success:
             await message.reply_text("𝖱𝖾𝗉𝗈𝗋𝗍𝖾𝖽 𝗍𝗈 𝖠𝖽𝗆𝗂𝗇𝗌!")
 
+# purge py
+
+@Client.on_message(
+    filters.command("purge", COMMAND_HAND_LER) &
+    f_onw_fliter
+)
+async def purge(client, message):
+    """ purge upto the replied message """
+    if message.chat.type not in (("supergroup", "channel")):
+        # https://t.me/c/1312712379/84174
+        return
+
+    is_admin = await admin_check(message)
+
+    if not is_admin:
+        return
+
+    status_message = await message.reply_text("...", quote=True)
+    await message.delete()
+    message_ids = []
+    count_del_etion_s = 0
+
+    if message.reply_to_message:
+        for a_s_message_id in range(
+            message.reply_to_message.message_id,
+            message.message_id
+        ):
+            message_ids.append(a_s_message_id)
+            if len(message_ids) == TG_MAX_SELECT_LEN:
+                await client.delete_messages(
+                    chat_id=message.chat.id,
+                    message_ids=message_ids,
+                    revoke=True
+                )
+                count_del_etion_s += len(message_ids)
+                message_ids = []
+        if len(message_ids) > 0:
+            await client.delete_messages(
+                chat_id=message.chat.id,
+                message_ids=message_ids,
+                revoke=True
+            )
+            count_del_etion_s += len(message_ids)
+
+    await status_message.edit_text(
+        f"deleted {count_del_etion_s} messages"
+    )
+    await asyncio.sleep(5)
+    await status_message.delete()
+
+# purge helper py
+
+USE_AS_BOT = os.environ.get("USE_AS_BOT", True)
+
+def f_sudo_filter(filt, client, message):
+    return bool(
+        message.from_user.id in AUTH_USERS
+    )
+
+
+sudo_filter = filters.create(
+    func=f_sudo_filter,
+    name="SudoFilter"
+)
+
+
+def onw_filter(filt, client, message):
+    if USE_AS_BOT:
+        return bool(
+            True # message.from_user.id in ADMINS
+        )
+    else:
+        return bool(
+            message.from_user and
+            message.from_user.is_self
+        )
+
+
+f_onw_fliter = filters.create(
+    func=onw_filter,
+    name="OnwFilter"
+)
+
+
+async def admin_filter_f(filt, client, message):
+    return await admin_check(message)
+
+
+admin_fliter = filters.create(
+    func=admin_filter_f,
+    name="AdminFilter"
+)
+
 
 
 
@@ -478,6 +571,6 @@ Thanks For Your Support...
 
 
 PPC = environ.get("PPC", "https://telegra.ph/file/3b6afd6c6fcd09606ea9f.jpg")
-
+COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
 TG_MAX_SELECT_LEN = environ.get("TG_MAX_SELECT_LEN", "100")
 
