490955c9bf7aee3bed1483cd227ca4abfad8cd9c
sahid malik <103699299+malik7983@users.noreply.github.com>
2022-07-20 14:06:42 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -3,7 +3,7 @@ import asyncio
 import re
 import ast
 
-from plugins.malik.extra import GHHMT, STTS, PPC
+from plugins.malik.extra import GHHMT, STTS, PPC, MOVIETET
 from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 from Script import script, ALURT_FND, M_NT_FND, M_NNT_FND, M_NNT_FNDD
 import pyrogram
@@ -972,7 +972,7 @@ async def advantage_spell_chok(msg):
     g_s += await search_gagala(msg.text)
     gs_parsed = []
     if not g_s:
-        k = await msg.reply("I couldn't find any movie in that name.")
+        k = await msg.reply(MOVIETET),
         await asyncio.sleep(8)
         await k.delete()
         return
@@ -1001,7 +1001,7 @@ async def advantage_spell_chok(msg):
     movielist += [(re.sub(r'(\-|\(|\)|_)', '', i, flags=re.IGNORECASE)).strip() for i in gs_parsed]
     movielist = list(dict.fromkeys(movielist))  # removing duplicates
     if not movielist:
-        k = await msg.reply("I couldn't find anything related to that. Check your spelling")
+        k = await msg.reply(MOVIETET),
         await asyncio.sleep(8)
         await k.delete()
         return
