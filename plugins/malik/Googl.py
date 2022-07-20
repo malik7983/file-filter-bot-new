97d7b53ec7b300af6204ffbcd76e1bf830e93cf3
sahid malik <103699299+malik7983@users.noreply.github.com>
2022-07-20 14:23:26 +0530

    Update Googl.py

--- plugins/malik/Googl.py
+++ plugins/malik/Googl.py
@@ -1,24 +1,8 @@
 
-#malik
-from pyrogram import Client, filters
-import re, random, asyncio 
-async def group_filters(client, update):
-    if re.findall("((^\/|^,|^!|^\.|^[\U0001F600-\U000E007F]).*)", update.text):
-        return
-    if 2 < len(update.text) < 100:    
-        btn = []
-        search = update.text
-        await get_settings(update.chat.id)
-        MOVIETET = settings["template"]
-        files = await get_filter_results(query=search)
-        if not files:
-            if settings["spellmode"]:
-                try:
-                    reply = search.replace(" ", '+')  
-                    buttons = [[ InlineKeyboardButton("🔍 𝚂𝙴𝙰𝚁𝙲𝙷 𝚃𝙾 𝙶𝙾𝙾𝙶𝙻𝙴 🔎", url=f"https://www.google.com/search?q={reply}") ],[ InlineKeyboardButton("× 𝙲𝙻𝙾𝚂𝙴 ×", callback_data="close") ]]
-                    spell = await update.reply_text(text=settings["spelltext"].format(query=search, first_name=update.from_user.first_name, last_name=update.from_user.last_name, title=update.chat.title, mention=update.from_user.mention), disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(buttons))           
-                    await asyncio.sleep(60)
-                    await spell.delete()
-                except:
-                    pass
-            return
+          
+MOVIETET = reply = search.replace(" ", '+')  
+   buttons = [[ InlineKeyboardButton("🔍 𝚂𝙴𝙰𝚁𝙲𝙷 𝚃𝙾 𝙶𝙾𝙾𝙶𝙻𝙴 🔎", url=f"https://www.google.com/search?q={reply}") ],[ InlineKeyboardButton("× 𝙲𝙻𝙾𝚂𝙴 ×", callback_data="close") ]]
+   spell = await update.reply_text(text=settings["spelltext"].format(query=search, first_name=update.from_user.first_name, last_name=update.from_user.last_name, title=update.chat.title, mention=update.from_user.mention), disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(buttons))           
+   await asyncio.sleep(60)
+   await spell.delete()
+     
