6cdfbd70543526449da03fb39ca505259c5ebec0
sahid malik <103699299+malik7983@users.noreply.github.com>
2022-07-22 23:01:00 +0530

    Update extra.py

--- plugins/malik/extra.py
+++ plugins/malik/extra.py
@@ -1,13 +1,10 @@
 #malik
-from pyrogram import (
-    filters
-)
 import re
 import os
 import shutil
 import pyrogram
 import asyncio
-from plugins.malik.extra import f_onw_fliter
+from plugins.malik.extra.cpp import f_onw_fliter
 from telegraph import upload_file
 from os import environ
 from info import PHT, ADMINS, AUTH_USERS
