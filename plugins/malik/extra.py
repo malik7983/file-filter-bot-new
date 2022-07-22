3def6914052a8eae5dfcbca7cd69caa868b6f4a9
sahid malik <103699299+malik7983@users.noreply.github.com>
2022-07-22 23:21:06 +0530

    Update extra.py

--- plugins/malik/extra.py
+++ plugins/malik/extra.py
@@ -516,6 +516,51 @@ async def telegraph(client, message):
             ignore_errors=True
         )
 
+
+# cpp py
+USE_AS_BOT = os.environ.get("USE_AS_BOT", True)
+
+def f_sudo_filter(filt, client, message):
+    return bool(
+        message.from_user.id in AUTH_USERS
+    )
+
+
+sudo_filter = filters.create(
+    func=f_sudo_filter,
+    name="SudoFilter"
+)
+
+
+def onw_filter(filt, client, message):
+    if USE_AS_BOT:
+        return bool(
+            True # message.from_user.id in ADMINS
+        )
+    else:
+        return bool(
+            message.from_user and
+            message.from_user.is_self
+        )
+
+
+f_onw_fliter = filters.create(
+    func=onw_filter,
+    name="OnwFilter"
+)
+
+
+async def admin_filter_f(filt, client, message):
+    return await admin_check(message)
+
+
+admin_fliter = filters.create(
+    func=admin_filter_f,
+    name="AdminFilter"
+)
+
+
+
 #get file id py
 
 def get_file_id(msg: Message):
