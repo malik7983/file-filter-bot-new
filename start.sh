3fe8217dbf67791fd64a670af0f1faffeb1d9f4d
sahid malik <103699299+malik7983@users.noreply.github.com>
2022-07-24 11:43:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/malik7983/file-filter-bot-new.git /file-filter-bot-new
+  git clone https://github.com/Sahidmalik07/movies-house-bot.git /movies-house-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /file-filter-bot-new
+  git clone $UPSTREAM_REPO /movies-house-bot
 fi
-cd /file-filter-bot-new
+cd /movies-house-bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot....💥"
 python3 bot.py
