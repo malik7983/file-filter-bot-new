a54dc136577d9ec195cea76948f87b91955a156b
sahid malik <103699299+malik7983@users.noreply.github.com>
2022-07-25 01:02:25 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1045,6 +1045,7 @@ async def advantage_spell_chok(msg):
         photo=(MQTTP),
         caption=(MQTTT.format(query)),
         reply_markup=reply_markup
+    else:
         reply_markup=InlineKeyboardMarkup(btn))    
     await asyncio.sleep(100) 
     await a.delete()
