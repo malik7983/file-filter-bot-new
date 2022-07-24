85812bd085c45cd0104ca89c590e77087c5b1fdd
sahid malik <103699299+malik7983@users.noreply.github.com>
2022-07-24 09:32:03 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2,13 +2,13 @@
 import asyncio
 import re
 import ast
-from plugins.malik.extra import GHHMT, STTS, MQTT, MQTTP, PPC, REPORT, PURGE, MUTE
+from plugins.malik.extra import GHHMT, SMART_PIC, STTS, MQTT, MQTTP, PPC, REPORT, PURGE, MUTE
 from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 from Script import script, ALURT_FND, M_NT_FND, M_NNT_FND, M_NNT_FNDD
 import pyrogram
 from database.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, \
     make_inactive
-from info import ADMINS, AUTH_CHANNEL, M_NT_F, AUTH_USERS, M_N_F, VIDEO_VD, SMART_PIC, CUSTOM_FILE_CAPTION, AUTH_GROUPS, P_TTI_SHOW_OFF, IMDB, \
+from info import ADMINS, AUTH_CHANNEL, M_NT_F, AUTH_USERS, M_N_F, VIDEO_VD, CUSTOM_FILE_CAPTION, AUTH_GROUPS, P_TTI_SHOW_OFF, IMDB, \
     SINGLE_BUTTON, SPELL_CHECK_REPLY, IMDB_TEMPLATE
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram import Client, filters
