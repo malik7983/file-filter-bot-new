b3ee9ed1e7e8dfb103bbf71c8993154035dd0d45
sahid malik <103699299+malik7983@users.noreply.github.com>
2022-07-25 00:20:11 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2,7 +2,7 @@
 import asyncio
 import re
 import ast
-from plugins.malik.extra import GHHMT, SMART_PIC, STTS, MQTT, MQTTP, PPC, REPORT, PURGE, MUTE
+from plugins.malik.extra import GHHMT, SMART_PIC, STTS, MQTT, MQTTT, MQTTP, PPC, REPORT, PURGE, MUTE
 from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 from Script import script, ALURT_FND, M_NT_FND, M_NNT_FND, M_NNT_FNDD
 import pyrogram
