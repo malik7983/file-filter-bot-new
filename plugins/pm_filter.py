5dcf17edacab70f6ffaf042046280afcdcc834b9
sahid malik <103699299+malik7983@users.noreply.github.com>
2022-07-25 23:07:11 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1044,9 +1044,8 @@ async def advantage_spell_chok(msg):
     ]]  
     )    
     m = await msg.reply(f"<b>Hey, {msg.from_user.mention}...😎\n\nCHECK YOUR MOVIE ON THE GIVEN LIST AND SELECT YOUR MOVIE.. \n\n दी गई सूची में अपनी फिल्म देखें और अपनी फिल्म चुनें 👇👇👇</b>",
-                    reply_markup=reply_markup
+                    reply_markup=reply_markup(btn))
     )
-        reply_markup=InlineKeyboardMarkup(btn))
 
 
 
