7476a3763f643676d91dc2892f528cb7f92a7e34
sahid malik <103699299+malik7983@users.noreply.github.com>
2022-07-25 07:51:47 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1034,20 +1034,22 @@ async def advantage_spell_chok(msg):
         )
     ] for k, movie in enumerate(movielist)]
     btn.append([InlineKeyboardButton(text="Close", callback_data=f'spolling#{user}#close_spellcheck')])
-    await msg.reply(
-                  text=(MQTTT.format(query)),
-                  reply_markup=InlineKeyboardMarkup(btn))
-    reply = query.replace(" ", '+')  
+    m = await msg.reply(f"<b>Hey, {msg.from_user.mention}...😎\n\nCHECK YOUR MOVIE ON THE GIVEN LIST AND SELECT YOUR MOVIE.. \n\n दी गई सूची में अपनी फिल्म देखें और अपनी फिल्म चुनें 👇👇👇</b>",
+                    reply_markup=InlineKeyboardMarkup(btn))
+    await asyncio.sleep(300)
+    await m.delete()
+    reply = query.replace(" ", '+')
     reply_markup = InlineKeyboardMarkup([[
     InlineKeyboardButton("🔍 Click To Check Spilling ✅", url=f"https://www.google.com/search?q={reply}")
     ],[
     InlineKeyboardButton("🔍 Click To Check Release Date 📅", url=f"https://www.google.com/search?q={reply}+release+date")
     ]]  
     )    
-    a = await msg.reply_text(
-        text=(MQTTT.format(query)),
-        reply_markup=reply_markup
-    )    
+    a = await msg.reply_photo(
+        photo=(MQTTP),
+        caption=(MQTT.format(msg.from_user.mention, query)),
+        reply_markup=reply_markup 
+    )                
     await asyncio.sleep(100) 
     await a.delete()
         
