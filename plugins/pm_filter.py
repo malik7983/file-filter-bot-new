4a9e0f264f2281a064fb0bf69f92192dba3b0446
sahid malik <103699299+malik7983@users.noreply.github.com>
2022-07-26 15:30:15 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2,7 +2,7 @@
 import asyncio
 import re
 import ast
-from plugins.malik.Gfilter import manual_filters
+from plugins.malik.Gfilter import manual_filters, advantage_spell_chok
 from plugins.malik.extra import GHHMT, SMART_PIC, STTS, MQTT, MQTTT, MQTTP, PPC, REPORT, PURGE, MUTE
 from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 from Script import script, ALURT_FND, M_NT_FND, M_NNT_FND, M_NNT_FNDD
